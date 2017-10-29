565ab6d2754b7b75ef75a24ecaa3fba905f71edb
gilesv <viniciusgcp@gmail.com>
2017-10-29 19:59:50 -0200

    Finish cadastro

--- js/site.js
+++ js/site.js
@@ -1,4 +1,11 @@
+/* Menu slide */
 $(document).ready(function() {
+    $('#salario').bind('keypress',mask.money);
+    $('#salario').bind('keyup',mask.money);
+    toggleMenu();
+});
+
+function toggleMenu() {
     $(".topnav-botao").click(function() {
         if($(".menu").hasClass("menu-show")) {
             $(".menu").removeClass("menu-show");
@@ -8,4 +15,69 @@ $(document).ready(function() {
             $("#sandbotao").addClass("topnav-twisted");
         }
     });
+}
+
+/* Validacao dos campos obrigatorios */
+function removeFillAlert(element) {
+    if($("#" + element.id).parent().hasClass("fill-alert")) {
+        $("#" + element.id).parent().removeClass("fill-alert");
+    }
+}
+
+$("form").submit(function (e) {
+    var requiredFields = $(".required");
+    var avoidSubmit = false;
+    var firstScrollValue = 0;
+    for(let i = 0; i < requiredFields.length; i++) {
+        var id = "#" + requiredFields[i].id;
+        
+        if(requiredFields[i].value == "" || requiredFields[i].value == "-" || requiredFields[i].value == "Selecione") {
+            if(i == 0 || firstScrollValue == 0) {
+                firstScrollValue = $(id).offset().top;
+            }
+            $(id).parent().addClass("fill-alert");
+            avoidSubmit = true;
+        }
+    }
+    if (avoidSubmit) {
+        e.preventDefault();
+        $(document).scrollTop(firstScrollValue - 80);
+    }
 });
+
+$("select").change(function(){
+    removeFillAlert(this);
+});
+$("input").change(function(){
+    removeFillAlert(this);
+});
+$("textarea").change(function(){
+    removeFillAlert(this);
+});
+
+/* Validacao do salario */
+var mask = {
+    money: function() {
+        var el = this;
+        exec = function(v) {
+            v = v.replace(/\D/g,"");
+            v = new String(Number(v));
+            var len = v.length;
+            if (len == 1) {
+                v = v.replace(/(\d)/,"R\$ 0,0$1");
+            }
+            else if (len == 2) {
+                v = v.replace(/(\d)/,"R\$ 0,$1");
+            } else if (len > 2 && len < 6) {
+                v = "R$ " + v.replace(/(\d{2})$/,',$1');
+            } else {
+                v = "R$ " + v.replace(/(\d)(\d{5})$/,"$1.$2").replace(/(\d)(\d{2})$/,"$1,$2");
+            }
+            return v;
+        };
+   
+        setTimeout(function(){
+            el.value = exec(el.value);
+        }, 1);
+    }
+}
