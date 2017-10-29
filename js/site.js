5765e71635872f12d7510e2c32cb1018a801705b
gilesv <viniciusgcp@gmail.com>
2017-10-29 17:07:13 -0200

    .

--- js/site.js
+++ js/site.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
     $(".topnav-botao").click(function() {
         if($(".menu").hasClass("menu-show")) {
             $(".menu").removeClass("menu-show");
+            $("#sandbotao").removeClass("topnav-twisted");
         } else {
             $(".menu").addClass("menu-show");
+            $("#sandbotao").addClass("topnav-twisted");
         }
     });
 });
